df94536580dac1ab1f2588fdca7414cba9766e68
Kwadwo Oteng-Amoako <49571112+KingsleyOteng@users.noreply.github.com>
2019-08-24 12:20:25 +0000

    Add files via upload
    Created an object to hold book details, class 'BookObject'. We then tied the object to TableView in FXML; it thus updates when we click the catalogue button.

--- FXMLDocumentController.java
+++ FXMLDocumentController.java
@@ -57,7 +57,9 @@ public class FXMLDocumentController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         employee.getItems().add(new LibraryBooksStatus(1, "kwadwo oteng-amoako", 2100, "Stepford Wives", "Ira Lee"));
         employee.getItems().add(new LibraryBooksStatus(2, "kofi oteng-boateng", 2000, "Wuthering Heights", "Emily Bronte"));
-
+        employee.getItems().add(new LibraryBooksStatus(1, "kwadwo oteng-amoako", 2100, "Stepford Wives", "Ira Lee"));
+        employee.getItems().add(new LibraryBooksStatus(2, "kofi oteng-boateng", 2000, "Wuthering Heights", "Emily Bronte"));
+        
         year.setCellValueFactory(new PropertyValueFactory<>("year"));
         id.setCellValueFactory(new PropertyValueFactory<>("id"));
         name.setCellValueFactory(new PropertyValueFactory<>("name"));
@@ -67,13 +69,39 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     private void borrowpage_button(ActionEvent event) {
-        System.out.println("You clicked me!");
+        System.out.println("You clicked me again!");
+        //BookObject buch = new BookObject();
+        //buch.setId(10);
+        //buch.setAuthor("God");
+        //buch.setBook_name("The Holy Bible");
+        //buch.setStudent_name("Christians");
+        //buch.setStudent_year(200);
+        
+        BookObject buch = new BookObject(10, "kwadwo", 2019, "The Holy Bible", "Christians");
+        this.insert_book_entry(buch);
         //label.setText("Hello World!");
         // fx_borrowpage_author.setText("author");
         //fx_borrowpage_title.setText("title");
-        //fx_borrowpage_published.setText("published");
-
+        //fx_borrowpage_published.setText("published")
         // System.out.println(fx_borrowpage_overview.getText());
     }
-
+    
+    
+    
+    /**
+    *
+    * @author kwadwooteng-amoako
+    * @date 24 August 2019
+    * @description following method takes a book object and inserts it into table view
+    *
+    */
+    @FXML
+    private void insert_book_entry(BookObject bucher)
+    {
+      employee.getItems().add(new LibraryBooksStatus(bucher.getId(),bucher.getStudent_name(),bucher.getStudent_year(),bucher.getBook_name(),bucher.getAuthor()));
+    
+  
+    
+   
+    }
 }
