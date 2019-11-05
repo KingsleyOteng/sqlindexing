7b58cfe35bcaa019769c845e493b870fde2c6a60
Kwadwo Oteng-Amoako <49571112+KingsleyOteng@users.noreply.github.com>
2019-11-05 09:50:16 +0000

    Add files via upload
    Bug fixes.

--- FXMLDocumentController.java
+++ FXMLDocumentController.java
@@ -1068,6 +1068,7 @@ public class FXMLDocumentController implements Initializable {
             Optional<ButtonType> result = alert.showAndWait();
 
             if (result.get() == ButtonType.OK) {
+                
                 // select_search_item_return();
                 cloud_search.setISBN1(obtain_ISBN());
 
@@ -1112,8 +1113,11 @@ public class FXMLDocumentController implements Initializable {
                 cloud_search.getThumbNailIndentifier()
         );
         
+        if (obtain_ISBN().equals(""))
+                {
         searchTableCatalogue.getItems().add(libObj);
-
+                }
+       
         System.out.println(cloud_search.getAuthor());
 
         JDBC_Controller jd = new JDBC_Controller();
@@ -1171,6 +1175,7 @@ public class FXMLDocumentController implements Initializable {
 
             // generate error message
             this.isbn_error();
+            val = "";
         }
 
         return val;
@@ -1246,6 +1251,7 @@ public class FXMLDocumentController implements Initializable {
         alert.setHeaderText("Error in the ISBN number. \n Book has not been added.");
 
         Optional<ButtonType> result = alert.showAndWait();
+       
 
     }
 
