899e505cd8c9ece23b680d783c3d9e53299f257f
Kwadwo Oteng-Amoako <49571112+KingsleyOteng@users.noreply.github.com>
2019-12-03 17:00:27 +0000

    Add files via upload
    Edits.

--- FXMLDocumentController.java
+++ FXMLDocumentController.java
@@ -1594,7 +1594,7 @@ public class FXMLDocumentController implements Initializable {
         for (int i = 1; i <=5;i++)
         {
             //Concatinate each loop 
-            fieldContent.append(fx_borrowpage_author.getText().toString() + " " + fx_borrowpage_title.getText().toString() );
+            fieldContent.append("Book: "+ fx_borrowpage_author.getText().toString() + " " + fx_borrowpage_title.getText().toString() );
         }
 
         book_trolley.setText (fieldContent.toString());
